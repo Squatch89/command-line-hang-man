c572b896d4debe37307514f096a1b3fd44ede1b4
Gene Williams <GeneWillb@Gmail.com>
2017-10-20 11:43:47 -0400

    word convereted to underscores

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
 //this is where prompt or inquirer will be
+const inquirer = require('inquirer');
+let guessesLeft = 10;
+
+function runGame() {
+    if (guessesLeft > 10 || /*something else related to word length */) {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Guess a Letter",
+                name: "userGuess"
+            }
+        ]).then(function (answers) {
+            console.log(answeres);
+            runGame();
+        });
+    }
+}
--- letter.js
+++ letter.js
@@ -1,5 +1,22 @@
-function Letter() {
+const wordChoices = ["Banana", "Apple", "Carrot", "Cheese", "Pear"];
+const wordToGuess = wordChoices[Math.floor(Math.random() * (wordChoices.length))];
 
+function Letter(randomWord) {
+    this.randomWord = function() {
+        console.log(wordToGuess);
+    
+        //this is going to take in the word and convert it to _ _ _ _ _
+        const underscore = Array.from('_'.repeat(wordToGuess.length));
+        console.log(underscore.join(' '));
+    }
+    
 }
+
+//creates a random number to select an index in my wordToGuess array
+
+
+const testLetter = new Letter();
+
+testLetter.randomWord();
 
 module.exports = Letter;
